1359d6264bec68863572015eedec7229c53c070d
TausifKhan <tausif.n.n.73n@gmail.com>
2025-07-26 16:44:36 +0530

    Add files via upload

---
+++ chapter 1/05_comments.c
@@ -0,0 +1,13 @@
+/*
+This is my first comment 
+author is harry 
+enjoying the coding journey
+*/
+#include<stdio.h>
+int main()
+{
+   // I am writting this just for fun. This line doesnt make any sense.
+   printf("hello world");
+   //Return o is returning o to the OS
+   return 0;//this is return
+}
